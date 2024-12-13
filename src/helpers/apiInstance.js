89247480f1be0f1eec1e0d9459c86960ae6b6c0a
Nir Simkin <nir.simkin@gmail.com>
2024-12-13 12:05:22 +0200

    test console.log

--- src/helpers/apiInstance.js
+++ src/helpers/apiInstance.js
@@ -3,4 +3,8 @@ import ApiService from "./apiService";
 const apiInstance = new ApiService(
   "https://backendserver-hqescvdecwarbbad.canadacentral-01.azurewebsites.net"
 );
+
+console.log(
+  "https://backendserver-hqescvdecwarbbad.canadacentral-01.azurewebsites.net"
+);
 export default apiInstance;
